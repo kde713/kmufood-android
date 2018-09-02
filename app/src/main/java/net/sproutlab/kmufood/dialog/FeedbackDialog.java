058f3d28388cb2f160f90e02b0db85c2ebbb1e81
kde713 <kde713@gmail.com>
2018-09-02 23:04:17 +0900

    피드백 다이알로그 수정 (연락수단)

--- app/src/main/java/net/sproutlab/kmufood/dialog/FeedbackDialog.java
+++ app/src/main/java/net/sproutlab/kmufood/dialog/FeedbackDialog.java
@@ -20,25 +20,20 @@ public class FeedbackDialog extends Dialog implements View.OnClickListener {
         this.c = context;
         setContentView(R.layout.dialog_feedback);
 
-        findViewById(R.id.btn_contact_kakaotalk).setOnClickListener(this);
         findViewById(R.id.btn_contact_email).setOnClickListener(this);
-        findViewById(R.id.btn_contact_facebook).setOnClickListener(this);
+        findViewById(R.id.btn_contact_github).setOnClickListener(this);
         findViewById(R.id.btn_close).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn_contact_kakaotalk:
-                c.startActivity(new Intent(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://plus.kakao.com/home/@sprout"))));
-                dismiss();
-                break;
             case R.id.btn_contact_email:
-                c.startActivity(new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:admin@sproutlab.net")));
+                c.startActivity(new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:kde713@kookmin.ac.kr")));
                 dismiss();
                 break;
-            case R.id.btn_contact_facebook:
-                c.startActivity(new Intent(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.facebook.com/sproutlab.net/"))));
+            case R.id.btn_contact_github:
+                c.startActivity(new Intent(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://github.com/kde713/kmufood-android"))));
                 dismiss();
                 break;
             case R.id.btn_close:
