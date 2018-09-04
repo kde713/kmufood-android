45e9f1791c98282b805f0de83072cd5d9c64842c
kde713 <kde713@gmail.com>
2018-09-04 15:52:12 +0900

    MultiDex Issue 수정

--- app/src/main/java/net/sproutlab/kmufood/KMUFoodApplication.java
+++ app/src/main/java/net/sproutlab/kmufood/KMUFoodApplication.java
@@ -1,11 +1,11 @@
 package net.sproutlab.kmufood;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * Created by kde713 on 2016. 9. 12..
  */
-public class KMUFoodApplication extends Application {
+public class KMUFoodApplication extends MultiDexApplication {
     private boolean isUpdateChecked = false;
 
     public boolean isUpdateChecked() {
