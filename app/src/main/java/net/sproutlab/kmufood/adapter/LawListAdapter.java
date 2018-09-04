cab29633da9751ab3ffe2d55019a02b9d4c6775f
Dongho Gang <gongdongho12@gmail.com>
2018-09-05 05:10:45 +0900

    한울식당 가격표기 동적 뷰 구현

--- app/src/main/java/net/sproutlab/kmufood/adapter/LawListAdapter.java
+++ app/src/main/java/net/sproutlab/kmufood/adapter/LawListAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import net.sproutlab.kmufood.R;
@@ -15,7 +16,9 @@ import net.sproutlab.kmufood.utils.MenuDataHelper;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -94,6 +97,15 @@ public class LawListAdapter extends PagerAdapter implements CardAdapter {
                 R.id.law_s6_price,
                 R.id.law_s7_price
         };
+        int[] areaset = {
+                R.id.append_area1,
+                R.id.append_area2,
+                R.id.append_area3,
+                R.id.append_area4,
+                R.id.append_area5,
+                R.id.append_area6,
+                R.id.append_area7
+        };
 
         ((TextView) view.findViewById(R.id.card_title)).setText(dayTitles[position]);
 
@@ -110,13 +122,64 @@ public class LawListAdapter extends PagerAdapter implements CardAdapter {
                     StringBuffer tmpPrice = new StringBuffer();
                     Pattern pattern = Pattern.compile("₩{1}\\d+/?\\d+");
                     Matcher m = pattern.matcher(menu);
+
                     while (m.find()) {
                         String temp = m.group();
                         menu = menu.replace("\n" + temp, "");
                         tmpPrice.append(temp.replace("₩", "") + "\n");
                     }
-                    ((TextView) view.findViewById(foodset[i])).setText(menu);
-                    ((TextView) view.findViewById(priceset[i])).setText(tmpPrice.toString());
+                    menu = menu.replaceAll("\n&", "&");
+                    Map<String, String> infoMap = new LinkedHashMap<String, String>();
+
+                    ArrayList<String> menuList = new ArrayList<String>();
+                    for (String lineMenu : menu.split("\n")) {
+                        //use filter
+                        if (!lineMenu.trim().isEmpty() && !lineMenu.trim().matches("^\\s+$")) {
+                            menuList.add(lineMenu.trim());
+                        }
+                    }
+
+                    ArrayList<String> priceList = new ArrayList<String>();
+                    for (String linePrice : tmpPrice.toString().split("\n")) {
+                        //use filter
+                        if (!linePrice.trim().isEmpty()) {
+                            priceList.add(linePrice.trim());
+                        }
+                    }
+
+                    for (int j = 0; j < menuList.size(); j++) {
+                        String lineMenu = menuList.get(j);
+                        //regex : [특수문자]아무거나한자리이상[특수문자]
+                        String spcharRegex = "^[^\\uAC00-\\uD7A3xfe0-9a-zA-Z\\\\s].+[^\\uAC00-\\uD7A3xfe0-9a-zA-Z\\\\s]$";
+                        String val = "";
+                        try {
+                            val = lineMenu.matches(spcharRegex) ? "" : priceList.remove(0);
+                        } catch (Exception e) {
+                        }
+                        infoMap.put(lineMenu, val);
+                    }
+
+                    if (infoMap.size() > 0) {
+                        //동적 뷰 할당
+                        LinearLayout area = (LinearLayout) view.findViewById(areaset[i]);
+                        area.removeAllViews();
+                        area.setOrientation(LinearLayout.VERTICAL);
+                        area.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 22f));
+                        TextView priceView = (TextView) view.findViewById(priceset[i]);
+                        priceView.setVisibility(View.GONE);
+
+                        for (Map.Entry<String, String> elem : infoMap.entrySet()) {
+                            LinearLayout item = (LinearLayout) LayoutInflater.from(container.getContext()).inflate(R.layout.food_item, container, false);
+                            TextView foodMenu = (TextView)item.findViewById(R.id.food_menu);
+                            foodMenu.setText(elem.getKey());
+                            TextView foodPrice = (TextView)item.findViewById(R.id.food_price);
+                            foodPrice.setText(elem.getValue());
+                            area.addView(item);
+                        }
+                    } else {
+                        ((TextView) view.findViewById(foodset[i])).setText(menu);
+                        ((TextView) view.findViewById(priceset[i])).setText(tmpPrice.toString());
+                    }
                 } else {
                     ((TextView) view.findViewById(foodset[i])).setText(menu);
                     ((TextView) view.findViewById(priceset[i])).setText(price);
